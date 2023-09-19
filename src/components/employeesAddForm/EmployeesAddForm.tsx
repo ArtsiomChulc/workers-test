79ca969e0f26530ea52ec7ae62996ca972f0fe14
ArtsiomChulc <artemorsha@mail.ru>
2023-09-20 00:04:48 +0300

    creating a project structure

---
+++ src/components/employeesAddForm/EmployeesAddForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EmployeesAddForm = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default EmployeesAddForm;
---
+++ src/components/employeesListItem/EmployeesListItem.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EmployeesListItem = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default EmployeesListItem;
